debf474feecd97ef66cd117108eff65a3ebc84fc
aeolian <aeolus.dev@protonmail.com>
2022-01-15 00:17:46 +0930

    WIP: copy updates for distributions

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -117,7 +117,7 @@ export default function Projects() {
 
           <a href="/#/create">
             <Button>
-              <Trans>New project</Trans>
+              <Trans>Create project</Trans>
             </Button>
           </a>
         </div>
